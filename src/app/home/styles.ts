7be3ca75827155125e97cc38ad77bd27e94a54ca
Pablo Alan <pabloxt14@gmail.com>
2025-07-18 18:18:52 -0300

    chore: organize project

---
+++ src/app/home/styles.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native'
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f4f4f5',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 20,
+    color: '#09090b',
+    fontWeight: 700,
+  },
+})
